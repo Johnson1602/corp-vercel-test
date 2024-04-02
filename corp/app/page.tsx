0f93bf2a5edf9653b4903c13ed734eba8c7206cb
Weiyi Xu <johnsonxwy@gmail.com>
2024-04-02 09:37:52 +0800

    refactor: 使用 layout 渲染 header

--- corp/app/page.tsx
+++ corp/app/page.tsx
@@ -1,18 +1,3 @@
-import { Fragment } from 'react'
-import Link from 'next/link'
-
 export default function HomePage() {
-  return (
-    <Fragment>
-      <header>
-        <nav>
-          <Link href='/performance'>Performance</Link>
-          <Link href='/reliability'>Reliability</Link>
-          <Link href='/scale'>Scale</Link>
-        </nav>
-      </header>
-
-      <h1>Home Page</h1>
-    </Fragment>
-  )
+  return <h1>Home Page</h1>
 }
