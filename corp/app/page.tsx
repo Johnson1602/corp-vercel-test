753280fa29a305d8b0c1433de8c37da51a1a41f3
Weiyi Xu <johnsonxwy@gmail.com>
2024-04-02 09:29:56 +0800

    feat: add header nav

--- corp/app/page.tsx
+++ corp/app/page.tsx
@@ -1,3 +1,18 @@
+import { Fragment } from 'react'
+import Link from 'next/link'
+
 export default function HomePage() {
-  return <h1>Home Page</h1>
+  return (
+    <Fragment>
+      <header>
+        <nav>
+          <Link href='/performance'>Performance</Link>
+          <Link href='/reliability'>Reliability</Link>
+          <Link href='/scale'>Scale</Link>
+        </nav>
+      </header>
+
+      <h1>Home Page</h1>
+    </Fragment>
+  )
 }
