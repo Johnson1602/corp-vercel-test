53498200edcfeadc3a03ba319e64c7748b8bcbee
Weiyi Xu <johnsonxwy@gmail.com>
2024-04-02 14:22:47 +0800

    feat: 在首页展示图片

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image'
+import homeImage from '@public/home.jpg'
+
 export default function HomePage() {
-  return <h1>Home Page</h1>
+  return (
+    <main className='absolute -z-10 inset-0'>
+      <h1>Home Page</h1>
+      <Image
+        src={homeImage}
+        alt='car factory'
+        fill
+        style={{ objectFit: 'cover', objectPosition: 'center' }}
+      />
+    </main>
+  )
 }
